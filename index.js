08e1c38173a9cb957aa64729ea4e50858e9e69dc
Hazeral <zturbin@gmail.com>
2020-05-16 16:40:43 +0100

    added 0 prefix to date function if less than 10

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function getDate() {
     const date = new Date();
-    const DD = date.getDate();
+    const DD = (date.getDate() < 10) ? ("0" + date.getDate()) : date.getDate();
     const MM = ((date.getMonth() + 1) < 10) ? ("0" + (date.getMonth() + 1)) : date.getMonth() + 1;
     const YYYY = date.getFullYear();
 
-    const HH = date.getHours();
-    const mm = date.getMinutes();
-    const ss = date.getSeconds();
+    const HH = (date.getHours() < 10) ? ("0" + date.getHours()) : date.getHours();
+    const mm = (date.getMinutes() < 10) ? ("0" + date.getMinutes()) : date.getMinutes();
+    const ss = (date.getSeconds() < 10) ? ("0" + date.getSeconds()) : date.getSeconds();
     
     return `${DD}-${MM}-${YYYY} ${HH}:${mm}:${ss}`;
 }
